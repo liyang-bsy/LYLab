6bc745cf6199bc2c12cbe1dedef196aec58839a7
Young <liyang_bsy@hotmail.com>
2015-06-25 16:08:08 +0800

    tc

--- src/net/vicp/lylab/utils/controller/TimeoutController.java
+++ src/net/vicp/lylab/utils/controller/TimeoutController.java
@@ -1,7 +1,9 @@
 package net.vicp.lylab.utils.controller;
 
-import java.util.Map;
-import java.util.WeakHashMap;
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import net.vicp.lylab.core.CoreDefine;
 import net.vicp.lylab.core.exception.LYException;
@@ -13,88 +15,78 @@ public final class TimeoutController extends Task {
 
 	private static Boolean init = false;
 	private static TimeoutController instance = null;
-
-	private Map<Recyclable, Integer> watchList = new WeakHashMap<Recyclable, Integer>();
 	
+	private List<WeakReference<Recyclable>> watchList = new ArrayList<WeakReference<Recyclable>>();
+
+	public static void init() {
+		if (!init) {
+			instance = new TimeoutController();
+			init = true;
+			instance.begin("TimeoutController");
+		}
+	}
+
 	@Override
 	public boolean isDaemon() {
 		return true;
 	}
-	
+
 	@Override
 	public void exec() {
 		try {
-			if (!init) {
-				instance = this;
-				init = true;
-			}
-			if (isStopped())
-				return;
 			while (!getThread().isInterrupted()) {
 				if (isStopped())
 					break;
-				Thread.sleep(CoreDefine.WAITING);
+				Thread.sleep(CoreDefine.WAITING_LONG);
 				getInstance().timeoutControl();
 			}
 		} catch (InterruptedException e) {
 			throw new LYException("TimeoutController is interrupted");
-		} finally {
-			instance = this;
-			init = true;
 		}
 	}
 
 	private void timeoutControl() {
 		System.gc();
-//		List<Task> callStopList = new LinkedList<Task>();
-//		List<Task> forceStopList = new LinkedList<Task>();
-		for(Recyclable rec : watchList.keySet())
+		Iterator<WeakReference<Recyclable>> iterator = watchList.iterator();
+		while(iterator.hasNext())
 		{
-			// skip myself
-			if(rec instanceof TimeoutController) continue;
-			if(isStopped() && !rec.isRecyclable()) continue;
+//			// skip myself
+//			if(rec instanceof TimeoutController) continue;
+			WeakReference<Recyclable> wref = iterator.next();
+			Recyclable rec = wref.get();
+			if(rec == null)
+			{
+				iterator.remove();
+				continue;
+			}
+			if(!rec.isRecyclable()) continue;
 			rec.recycle();
 		}
-//		for(Task task : LYTaskQueue.getThreadPool())
-//		{
-//			// skip WatchDog itself
-//			if(task instanceof WatchDog) continue;
-//			if(isStopped()) continue;
-//			// -1L means infinite
-//			if()
-//				continue;
-//		}
-//		for(Task task : forceStopList)
-//		{
-//			task.forceStop();
-//			if(task.getRetryCount() > 0)
-//				task.recycle();
-//			else log.error("Timeout task was killed:\n" + task.toString());
-//		}
-//		for(Task task : callStopList)
-//		{
-//			task.callStop();
-//			log.info("Try to stop timeout task:" + task.getTaskId());
-//		}
 	}
-	
-	public synchronized static boolean addToWatch(Recyclable rec)
-	{
-		if(getInstance().getState() == Task.BEGAN)
-			getInstance().begin();
-		return getInstance().getWatchList().put(rec, CoreDefine.DEFAULT_TOLERANCE) != null;
+
+	public synchronized static boolean addToWatch(Recyclable rec) {
+		return getInstance().getWatchList().add(new WeakReference<Recyclable>(rec));
+	}
+
+	public synchronized static void removeFromWatch(Recyclable rec) {
+		getInstance().getWatchList().remove(rec);
 	}
 
 	public static TimeoutController getInstance() {
+		if (instance == null)
+			init();
 		return instance;
 	}
 
-	public static void setInstance(TimeoutController instance) {
-		TimeoutController.instance = instance;
+	private List<WeakReference<Recyclable>> getWatchList() {
+		return watchList;
 	}
 
-	private Map<Recyclable, Integer> getWatchList() {
-		return watchList;
+	@Override
+	protected void aftermath() {
+		init = false;
+		instance = null;
+		super.aftermath();
 	}
 
 }
