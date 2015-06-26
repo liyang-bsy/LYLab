41c99d4cbe030d8f5c98d7e7eb8092eb0003a034
Young <liyang_bsy@hotmail.com>
2015-06-26 16:56:30 +0800

    version 1.0.0

--- src/net/vicp/lylab/core/CoreDefine.java
+++ src/net/vicp/lylab/core/CoreDefine.java
@@ -1,7 +1,18 @@
 package net.vicp.lylab.core;
 
+/**
+ * LYLab - powered by Young Lee's Lab.
+ * <br><br>
+ * Release Under GNU Lesser General Public License (LGPL).
+ * 
+ * @author Young Lee
+ * @since 2015.06.26
+ * @version 1.0.0
+ * 
+ */
 public class CoreDefine {
-
+	public static final String VERSION = "1.0.0";
+	
 	public static final long WAITING = 1000L;
 	public static final long WAITING_SHORT = 100L;
 	public static final long WAITING_LONG = 3000L;
