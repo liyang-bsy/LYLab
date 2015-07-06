96a162f823d3e689943d4a1d47b908457674ecdb
Young <liyang_bsy@hotmail.com>
2015-07-06 11:01:58 +0800

    Add space

--- src/net/vicp/lylab/utils/internet/ClientLongSocket.java
+++ src/net/vicp/lylab/utils/internet/ClientLongSocket.java
@@ -12,7 +12,7 @@ package net.vicp.lylab.utils.internet;
  */
 public class ClientLongSocket extends LongSocket {
 	private static final long serialVersionUID = -2811199603276510531L;
-
+	
 	public ClientLongSocket(String host, Integer port, HeartBeat heartBeat) {
 		super(host, port, heartBeat);
 	}
