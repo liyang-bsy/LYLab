a0011209a0cf96374e4ea6b4dad9c73aa34a7243
Young Lee <Young Lee@SecurityCenter>
2015-07-19 01:39:48 +0800

    how to use reserved data?!

--- src/net/vicp/lylab/utils/internet/async/ClientRead.java
+++ src/net/vicp/lylab/utils/internet/async/ClientRead.java
@@ -8,6 +8,7 @@ import java.nio.channels.SocketChannel;
 
 import net.vicp.lylab.core.interfaces.Protocol;
 import net.vicp.lylab.utils.internet.impl.LYLabProtocol;
+import net.vicp.lylab.utils.internet.impl.Message;
 
 public class ClientRead implements Runnable {
 	private Selector selector;
@@ -15,6 +16,19 @@ public class ClientRead implements Runnable {
 	public ClientRead(Selector selector) {
 		this.selector = selector;
 		new Thread(this).start();
+	}
+	
+	int lid=-1,id=-1;
+	
+	byte[] reserved = new byte[] { };
+	public void reserve()
+	{
+		;
+	}
+	
+	public void useKept()
+	{
+		;
 	}
 
 	public void run() {
@@ -27,16 +41,31 @@ public class ClientRead implements Runnable {
 					if (sk.isReadable()) {
 						// 使用NIO读取Channel中的数据
 						SocketChannel sc = (SocketChannel) sk.channel();
-						ByteBuffer buffer = ByteBuffer.allocate(1024);
-						sc.read(buffer);
+						ByteBuffer buffer = ByteBuffer.allocate(10240);
+						int len = sc.read(buffer);
+						//^^^^^^-------------
 						buffer.flip();
 						// 将字节转化为为UTF-16的字符串
+						//<<-------------
 						byte[] ret = buffer.array();
-						String receivedString = protocol.decode(ret).toString();
+						int start = 0, next = 0;
+						while((next = protocol.validate(ret, start, len))!=0)
+						{
+						buffer.get(ret, 0, next - start);
+						start = next;
+						Object o =protocol.decode(ret);
+						id ++;
+						if(lid+1!=id) System.out.println(lid+"\t"+id);
+						lid = ((Message) o).getMsgId();
+//						String receivedString = o.toString();
 						// 控制台打印出来
-						System.out.println("接收到来自服务器"
-								+ sc.socket().getRemoteSocketAddress() + "的信息:"
-								+ receivedString);
+//						System.out.println("接收到来自服务器"
+//								+ sc.socket().getRemoteSocketAddress() + "的信息:"
+//								+ receivedString);
+						}
+						if(buffer.hasRemaining())
+							buffer.get(ret, 0, next - start);
+						//<<-------------
 						// 为下一次读取作准备
 						sk.interestOps(SelectionKey.OP_READ);
 					}
