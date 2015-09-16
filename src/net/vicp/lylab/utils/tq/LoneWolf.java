f5e7f9aa180ce47751c1930a8773c77b040fb12e
Young <liyang_bsy@hotmail.com>
2015-09-16 15:46:20 +0800

    Set timeout to 0 in constructor

--- src/net/vicp/lylab/utils/tq/LoneWolf.java
+++ src/net/vicp/lylab/utils/tq/LoneWolf.java
@@ -6,6 +6,10 @@ import net.vicp.lylab.core.exceptions.LYException;
 public abstract class LoneWolf extends Task {
 	private static final long serialVersionUID = -8596789769206129576L;
 
+	public LoneWolf() {
+		timeout = 0;
+	}
+	
 	@Override
 	public BaseObject clone() {
 		throw new LYException("Clone is not supported for a lone wolf");
