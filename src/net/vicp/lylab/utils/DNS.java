7cd813da1af34d352ed7331bc32ff7eee6ab1c6a
Young <liyang_bsy@hotmail.com>
2015-09-25 16:19:10 +0800

    dns utils

--- src/net/vicp/lylab/utils/DNS.java
+++ src/net/vicp/lylab/utils/DNS.java
@@ -2,17 +2,31 @@ package net.vicp.lylab.utils;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Map;
 
-public class DNS {
+import net.vicp.lylab.core.CloneableBaseObject;
+
+public class DNS extends CloneableBaseObject {
+	protected static Map<String, String> DNSCache;
+	
 	public static void main(String[] args) throws UnknownHostException {
 		System.out.println(InetAddress.getByName("www.baidu.com"));
 		System.out.println(InetAddress.getByName("www.baidu.com").getHostAddress());
 		System.out.println(domainNameService("http://www.baidu.com/fdsaewq"));
+		System.out.println(isIP(domainNameService("http://www.ebaolife.com/fdsaewq")));
+		System.out.println(isIP("192.168.0.12201212"));
+		System.out.println(isIP("222.16.228.2ba"));
 	}
 	
-	public static String domainNameService(String domain) throws UnknownHostException
-	{
+	public static String domainNameService(String domain) throws UnknownHostException {
 		domain = domain.replaceAll("^[a-zA-Z]*://", "").replaceAll("/[\\S]*", "");
-		return InetAddress.getByName(domain).getHostAddress();
+		String ip = InetAddress.getByName(domain).getHostAddress();
+		return ip;
 	}
+
+	public static boolean isIP(String ipAddr) {
+		return ipAddr.matches(
+				"((?:(?:25[0-5]|2[0-4]\\d|((1\\d{2})|([1-9]?\\d)))\\.){3}(?:25[0-5]|2[0-4]\\d|((1\\d{2})|([1-9]?\\d))))");
+	}
+	
 }
