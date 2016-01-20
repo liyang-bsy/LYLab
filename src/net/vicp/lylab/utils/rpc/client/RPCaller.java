90292d46dec0642dbe4cc7e1ac8c302113c85a10
Young <liyang_bsy@hotmail.com>
2016-01-20 16:32:13 +0800

    fix bugs

--- src/net/vicp/lylab/utils/rpc/client/RPCaller.java
+++ src/net/vicp/lylab/utils/rpc/client/RPCaller.java
@@ -10,22 +10,25 @@ import net.vicp.lylab.core.interfaces.Protocol;
 import net.vicp.lylab.core.model.Message;
 import net.vicp.lylab.core.model.RPCMessage;
 import net.vicp.lylab.core.pool.AutoGeneratePool;
+import net.vicp.lylab.utils.atomic.AtomicBoolean;
 import net.vicp.lylab.utils.creator.AutoCreator;
 import net.vicp.lylab.utils.creator.InstanceCreator;
 import net.vicp.lylab.utils.internet.ClientLongSocket;
 import net.vicp.lylab.utils.operation.KeepAliveValidator;
 
 public class RPCaller extends NonCloneableBaseObject implements LifeCycle {
-	AutoGeneratePool<ClientLongSocket> pool = null;
-	AutoCreator<ClientLongSocket> creator = null;
+	private AutoGeneratePool<ClientLongSocket> pool = null;
+	private AutoCreator<ClientLongSocket> creator = null;
+	private boolean backgroundServer = true;
+	private AtomicBoolean closed = new AtomicBoolean(false);
 
-	public Message call(RPCMessage content) {
-		if (StringUtils.isBlank(content.getRpcKey()))
-			content.setRpcKey("RPC");
+	public Message call(RPCMessage message) {
+		if (StringUtils.isBlank(message.getRpcKey()))
+			message.setRpcKey("RPC");
 		Protocol p = (Protocol) CoreDef.config.getObject("protocol");
 		ClientLongSocket cls = pool.accessOne();
 		byte[] req, res;
-		req = p.encode(content);
+		req = p.encode(message);
 		res = cls.request(req);
 		pool.recycle(cls);
 		return (Message) p.decode(res);
@@ -33,30 +36,47 @@ public class RPCaller extends NonCloneableBaseObject implements LifeCycle {
 
 	@Override
 	public void initialize() {
-		creator = new InstanceCreator<ClientLongSocket>(ClientLongSocket.class, CoreDef.config.getString("rpcHost"),
-				CoreDef.config.getInteger("rpcPort"), CoreDef.config.getObject("protocol"),
-				CoreDef.config.getObject("heartBeat"));
-		pool = new AutoGeneratePool<ClientLongSocket>(creator, new KeepAliveValidator<ClientLongSocket>(), 20000,
-				Integer.MAX_VALUE);
+		if (closed.compareAndSet(false, true)) {
+			creator = new InstanceCreator<ClientLongSocket>(ClientLongSocket.class, CoreDef.config.getString("rpcHost"),
+					CoreDef.config.getInteger("rpcPort"), CoreDef.config.getObject("protocol"),
+					CoreDef.config.getObject("heartBeat"));
+			pool = new AutoGeneratePool<ClientLongSocket>(creator, new KeepAliveValidator<ClientLongSocket>(), 20000,
+					Integer.MAX_VALUE);
 
-		RPCMessage content = new RPCMessage();
-		content.setRpcKey("RegisterServer");
-		content.getBody().put("server", CoreDef.config.getString("server"));
-		content.getBody().put("procedures", CoreDef.config.getConfig("Aop").keyList());
-		Message m = call(content);
-		if (m.getCode() != 0)
-			throw new LYException("RPC Server register failed:\n" + m.toString());
+			if (isBackgroundServer()) {
+				RPCMessage message = new RPCMessage();
+				message.setRpcKey("RegisterServer");
+				message.getBody().put("server", CoreDef.config.getString("server"));
+				message.getBody().put("port", CoreDef.config.getInteger("port"));
+				message.getBody().put("procedures", CoreDef.config.getConfig("Aop").keyList());
+				Message m = call(message);
+				if (m.getCode() != 0)
+					throw new LYException("RPC Server register failed:\n" + m.toString());
+			}
+		}
 	}
 
 	@Override
-	public void close() throws Exception {
-		RPCMessage content = new RPCMessage();
-		content.setRpcKey("RemoveServer");
-		content.getBody().put("server", CoreDef.config.getString("server"));
-		content.getBody().put("procedures", CoreDef.config.getConfig("Aop").keyList());
-		call(content);
+	public void close() {
+		if (closed.compareAndSet(true, false)) {
+			if (isBackgroundServer()) {
+				RPCMessage message = new RPCMessage();
+				message.setRpcKey("RemoveServer");
+				message.getBody().put("server", CoreDef.config.getString("server"));
+				message.getBody().put("procedures", CoreDef.config.getConfig("Aop").keyList());
+				call(message);
+			}
 
-		pool.close();
+			pool.close();
+		}
+	}
+
+	public boolean isBackgroundServer() {
+		return backgroundServer;
+	}
+
+	public void setBackgroundServer(boolean backgroundServer) {
+		this.backgroundServer = backgroundServer;
 	}
 
 }
