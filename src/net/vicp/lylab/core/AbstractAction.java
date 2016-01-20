63d9849dc0ac1d3299f485a92028eb3784a9ca60
Young <liyang_bsy@hotmail.com>
2016-01-20 11:22:23 +0800

    base action changed

---
+++ src/net/vicp/lylab/core/AbstractAction.java
@@ -0,0 +1,49 @@
+package net.vicp.lylab.core;
+
+import java.net.Socket;
+
+import net.vicp.lylab.core.interfaces.Executor;
+
+/**
+ * An abstract Action, Override exec() to do your service
+ * @author Young
+ *
+ */
+public abstract class AbstractAction extends CloneableBaseObject implements Executor {
+	
+	protected Socket clientSocket;
+	protected Object request;
+	protected Object response;
+
+	public AbstractAction() { }
+	
+	public AbstractAction(Object request, Object response) {
+		this.request = request;
+		this.response = response;
+	}
+
+	public AbstractAction(AbstractAction batonPass) {
+		this(batonPass.getRequest(), batonPass.getResponse());
+	}
+
+	public Object getRequest() {
+		return request;
+	}
+
+	public void setRequest(Object request) {
+		this.request = request;
+	}
+
+	public Object getResponse() {
+		return response;
+	}
+
+	public void setResponse(Object response) {
+		this.response = response;
+	}
+
+	public void setSocket(Socket clientSocket) {
+		this.clientSocket = clientSocket;
+	}
+
+}
--- src/net/vicp/lylab/core/BaseAction.java
+++ src/net/vicp/lylab/core/BaseAction.java
@@ -1,50 +1,22 @@
 package net.vicp.lylab.core;
 
-import java.net.Socket;
-
-import net.vicp.lylab.core.interfaces.Executor;
 import net.vicp.lylab.core.model.Message;
 
 /**
- * BaseAction is a abstract Action, Override exec() to do your service
+ * BaseAction from abstract Action, Override exec() to do your service
  * @author Young
  *
  */
-public abstract class BaseAction extends CloneableBaseObject implements Executor {
-	
-	protected Socket clientSocket;
-	protected Message request;
-	protected Message response;
+public abstract class BaseAction extends AbstractAction {
 
-	public BaseAction() { }
-	
-	public BaseAction(Message request, Message response) {
-		this.request = request;
-		this.response = response;
-	}
-
-	public BaseAction(BaseAction batonPass) {
-		this(batonPass.getRequest(), batonPass.getResponse());
-	}
-
+	@Override
 	public Message getRequest() {
-		return request;
+		return (Message) super.getRequest();
 	}
 
-	public void setRequest(Message request) {
-		this.request = request;
-	}
-
+	@Override
 	public Message getResponse() {
-		return response;
-	}
-
-	public void setResponse(Message response) {
-		this.response = response;
-	}
-
-	public void setSocket(Socket clientSocket) {
-		this.clientSocket = clientSocket;
+		return (Message) super.getResponse();
 	}
 
 }
