e510794f35e91955f183aedfc1cb6a4218644454
Young <liyang_bsy@hotmail.com>
2016-01-28 13:09:44 +0800

    final

--- src/net/vicp/lylab/utils/DNS.java
+++ src/net/vicp/lylab/utils/DNS.java
@@ -8,25 +8,16 @@ import net.vicp.lylab.core.CloneableBaseObject;
 
 public abstract class DNS extends CloneableBaseObject {
 	protected static Map<String, String> DNSCache;
-	
-	public static void main(String[] args) throws UnknownHostException {
-		System.out.println(InetAddress.getByName("www.baidu.com"));
-		System.out.println(InetAddress.getByName("www.baidu.com").getHostAddress());
-		System.out.println(domainNameService("http://www.baidu.com/fdsaewq"));
-		System.out.println(isIP(domainNameService("http://www.ebaolife.com/fdsaewq")));
-		System.out.println(isIP("192.168.0.12201212"));
-		System.out.println(isIP("222.16.228.2ba"));
-	}
-	
-	public static String domainNameService(String domain) throws UnknownHostException {
+
+	public final static String domainNameService(String domain) throws UnknownHostException {
 		domain = domain.replaceAll("^[a-zA-Z]*://", "").replaceAll("/[\\S]*", "");
 		String ip = InetAddress.getByName(domain).getHostAddress();
 		return ip;
 	}
 
-	public static boolean isIP(String ipAddr) {
+	public final static boolean isIP(String ipAddr) {
 		return ipAddr.matches(
 				"((?:(?:25[0-5]|2[0-4]\\d|((1\\d{2})|([1-9]?\\d)))\\.){3}(?:25[0-5]|2[0-4]\\d|((1\\d{2})|([1-9]?\\d))))");
 	}
-	
+
 }
