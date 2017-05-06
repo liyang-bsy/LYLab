5b1081bd3b187faf410066b5d29d69578acc3ce1
Young Lee <Young Lee@SecurityCenter>
2017-05-06 11:27:17 +0800

    findOne经过斟酌，以不发生异常优先

--- src/main/java/net/vicp/lylab/mongodb/MongoDBService.java
+++ src/main/java/net/vicp/lylab/mongodb/MongoDBService.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.bson.Document;
 import org.bson.types.ObjectId;
 
+import net.vicp.lylab.core.NonCloneableBaseObject;
 import net.vicp.lylab.core.model.OrderBy;
 import net.vicp.lylab.core.model.Page;
 
@@ -20,7 +21,7 @@ import com.mongodb.client.result.UpdateResult;
  * @author liyang
  * @version 0.0.1
  */
-public class MongoDBService {
+public class MongoDBService extends NonCloneableBaseObject {
 
 	MongoCollection<Document> mongoCollection;
 
@@ -47,19 +48,41 @@ public class MongoDBService {
 		}
 		return list;
 	}
+	
+	private Map<String, Object> returnFirst(List<? extends Map<String, Object>> list, Map<String, Object> document) {
+		if (list == null || list.size() == 0) {
+			return null;
+		}
+		if (list.size() > 1) {
+			log.error("该条件查出重复对象：" + String.valueOf(document));
+		}
+		return list.get(0);
+	}
 
 	// 查询除重
 	public List<?> distinct(String fieldName) {
 		return iterable2List(mongoCollection.distinct(fieldName, Document.class));
 	}
 
+	// 按id查询(单查)
+	public Map<String, Object> findById(String id) {
+		Document tempFilter = new Document();
+		tempFilter.put("_id", new ObjectId(id));
+		return returnFirst(find(tempFilter), tempFilter);
+	}
+
+	// 条件查询(单查)
+	public Map<String, Object> findOne(Map<String, Object> filter) {
+		return returnFirst(find(filter, new Page(), new OrderBy("_id", "desc")), filter);
+	}
+
 	// 条件查询
-	public List<?> find(Map<String, Object> filter) {
+	public List<Map<String, Object>> find(Map<String, Object> filter) {
 		return find(filter, new Page(), new OrderBy("_id", "desc"));
 	}
 
 	// 条件查询
-	public List<?> find(Map<String, Object> filter, Page page, OrderBy orderBy) {
+	public List<Map<String, Object>> find(Map<String, Object> filter, Page page, OrderBy orderBy) {
 		Document tempFilter = new Document(filter);
 		// FIXME 排序暂时没写
 		return iterable2List(mongoCollection.find(tempFilter).sort(new Document()).skip(page.getIndex()).limit(page.getPageSize()));
@@ -85,26 +108,26 @@ public class MongoDBService {
 		mongoCollection.insertMany(list);
 	}
 
-	public UpdateResult update(String id, Map<String, Object> update) {
+	public UpdateResult updateById(String id, Map<String, Object> update) {
 		Document filter = new Document();
 		filter.put("_id", new ObjectId(id));
 		Document tempUpdate = new Document(update);
 		return mongoCollection.updateOne(filter, tempUpdate);
 	}
 
-	public UpdateResult updateMany(Map<String, Object> filter, Map<String, Object> update) {
+	public UpdateResult update(Map<String, Object> filter, Map<String, Object> update) {
 		Document tempFilter = new Document(filter);
 		Document tempUpdate = new Document(update);
 		return mongoCollection.updateMany(tempFilter, tempUpdate);
 	}
 
-	public DeleteResult delete(String id) {
+	public DeleteResult deleteById(String id) {
 		Document filter = new Document();
 		filter.put("_id", new ObjectId(id));
 		return mongoCollection.deleteOne(filter);
 	}
 
-	public DeleteResult deleteMany(Map<String, Object> filter) {
+	public DeleteResult delete(Map<String, Object> filter) {
 		Document tempFilter = new Document(filter);
 		return mongoCollection.deleteMany(tempFilter);
 	}
